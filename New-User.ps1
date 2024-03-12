c32abb94558d13e671127ac2089917afda778b9d
Magi501 <mike.swanson3@outlook.com>
2024-03-12 09:55:52 -0700

    Automatic Eventlog Pull

--- New-User.ps1
+++ New-User.ps1
@@ -1,8 +1,14 @@
-$givenName = "Henry"
-$surname = "Coombs"
-$samAccountName = "hcoombs"
-$userPrincipalName = "$samAccountName@contoso.com"
-$password = ConvertTo-SecureString "P@55w.rd" -AsPlainText -Force
-$ou = "OU=Users,DC=contoso,DC=com"
+    {
+    $givenName = "Henry"
+    $surname = "Coombs"
+    $samAccountName = "hcoombs"
+    $userPrincipalName = "$samAccountName@contoso.com"
+    $password = ConvertTo-SecureString "Pa55w.rd" -AsPlainText -Force
+    $ou = "OU=Users,DC=contoso,DC=com"
+    
+    New-ADUser -Name "$givenName $surname" -GivenName $givenName -Surname $surname -SamAccountName $samAccountName -UserPrincipalName $userPrincipalName -AccountPassword $password -Enabled $true -Path $ou
+    
+    
+    return $CompletionResults
 
-New-ADUser -Name "$givenName $surname" -GivenName $givenName -Surname $surname -SamAccountName $samAccountName -UserPrincipalName $userPrincipalName -AccountPassword $password -Enabled $true -Path $ou
+    }
--- Remote Restart.ps1
+++ Remote Restart.ps1
@@ -3,3 +3,11 @@ $computerName = "LON-DC1"
 $credential = Get-Credential
 
 Restart-Computer -ComputerName $computerName -Credential $credential -Force
+
+
+#Restart Computer, with embedded credntials
+$computerName = "LON-DC1"
+$credential = Import-Clixml -Path "C:\admincred.xml"
+
+Restart-Computer -ComputerName $computerName -Credential $credential -Force
+Write-Host "$computerName is restarting..."
