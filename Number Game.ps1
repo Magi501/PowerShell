7cfb83333c0ab6b2755520ac6d9f3941bbbcd560
Magi501 <mike.swanson3@outlook.com>
2024-03-13 13:42:25 -0700

    Added Number Games

---
+++ Number Game.ps1
@@ -0,0 +1,17 @@
+$number = Get-Random -Minimum 1 -Maximum 101
+Write-Host "Pick a number between 1 and 100. You have 7 attempts to get it right."
+
+for ($i = 1; $i -le 7; $i++) {
+        $guess = Read-Host "Guess #$i"
+
+    if ($guess -eq $number) {
+            Write-Host "Congratulations! You got ity in $i guesses."
+                    break
+ }
+     elseif ($guess -gt $number) {
+                Write-Host "Too high! You have $($i - 7) guesses left."
+     }
+         else {
+                    Write-Host "Too low! You have $($i - 7) guesses left."
+         }
+        }
--- Scratchpaper.ps1
+++ Scratchpaper.ps1
@@ -1,9 +1,2 @@
 
-$OU = "OU=Marketing,DC=Adatum,DC=com"
-$Department = Read-Host -Prompt "Enter Department"
-$Title = Read-Host "Enter Job Title"
-$Firstname = Read-Host "Enter First Name"
-$Lastname = Read-Host "Enter Last Name"
-$Username = $Firstname+"."+$Lastname
-$Password = Read-Host -AsSecureString -Prompt "Set Password"
 
