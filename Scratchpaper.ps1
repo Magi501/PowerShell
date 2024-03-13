dd244c12a68f91a9bfae9ad55789c6c5e91d73d6
Magi501 <mike.swanson3@outlook.com>
2024-03-13 11:54:03 -0700

    GUI work

--- Scratchpaper.ps1
+++ Scratchpaper.ps1
@@ -1,1 +1,9 @@
-Invoke-Command 
+
+$OU = "OU=Marketing,DC=Adatum,DC=com"
+$Department = Read-Host -Prompt "Enter Department"
+$Title = Read-Host "Enter Job Title"
+$Firstname = Read-Host "Enter First Name"
+$Lastname = Read-Host "Enter Last Name"
+$Username = $Firstname+"."+$Lastname
+$Password = Read-Host -AsSecureString -Prompt "Set Password"
+
