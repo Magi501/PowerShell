c9fd1669accdc1e86c881b9a4cc4bfb216d7346f
Magi501 <mike.swanson3@outlook.com>
2024-03-13 14:36:35 -0700

    puzzle fix

---
+++ Riddle Script.ps1
@@ -0,0 +1,11 @@
+$answer = "time"
+Write-Host "I am a river that forever flows, my outer banks endlessly erode, my depths measure to the beat of a metranome."
+for ($i = 1; $i -le 1; $i++) {
+    $guess = Read-Host "What am I?"
+    If ($guess -eq $answer) {
+        Read-Host "Congratulations, you have solved the riddle!"
+    }
+            elseif ($guess -ne $answer) {
+                Read-Host "Your answer is incorrect."
+    }
+}
