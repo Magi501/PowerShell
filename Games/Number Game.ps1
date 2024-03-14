9ef3a0fe30c83979caefd6803770ca5cc535f64d
Magi501 <mike.swanson3@outlook.com>
2024-03-14 11:01:03 -0700

    edit synch

--- Games/Number Game.ps1
+++ Games/Number Game.ps1
@@ -1,6 +1,12 @@
+#Welcome to the number game!
+#How this works: The computer generates a random number from 1-100 (101 to allow for 100 to be an option)\
+#Rules: 7 tries to guess the right number
+
+#This specifies the range 1-101 to allow "100" to be a possibility
 $number = Get-Random -Minimum 1 -Maximum 101
 Write-Host "Pick a number between 1 and 100. You have 7 attempts to get it right."
 
+# -le value is number of tries. I am using (7) in this setup
 for ($i = 1; $i -le 7; $i++) {
         $guess = Read-Host "Guess #$i"
 
--- Games/Riddle Script.ps1
+++ Games/Riddle Script.ps1
@@ -1,6 +1,14 @@
+#Welcome to the riddle game
+#This is a practice of the If Else logic.
+
+#The $answer variable is the answer
+
 $answer = "time"
+
+#The Riddle is below ""
 Write-Host "I am a river that forever flows, my outer banks endlessly erode, my depths measure to the beat of a metranome."
 for ($i = 1; $i -le 1; $i++) {
+#$guess is the prompt to request the answer from the one playing the game
     $guess = Read-Host "What am I?"
     If ($guess -eq $answer) {
         Read-Host "Congratulations, you have solved the riddle!"
